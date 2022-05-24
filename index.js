ead3c7127afa05fa66a5dffdb0a229a49361f97c
Ryan Saldaen <ryan.saldaen@gmail.com>
2022-05-24 01:46:05 -0700

    files named, models made

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
